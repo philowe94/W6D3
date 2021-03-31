650f83e519c73d5142ac353bad9ef2b5e9c517a1
akerpelm <akerpelm@u.rochester.edu>
2021-03-31 15:28:31 -0400

    Complete routes_controllers

--- RoutesControllers/app/controllers/users_controller.rb
+++ RoutesControllers/app/controllers/users_controller.rb
@@ -1,13 +1,50 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
     
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
     end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.delete
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+
+
 end
+
+# def update
+#     if song = song.find(params[:id]
+#         redirect_to song_url(song.id)
+# else 
+#     errors
